1a1014b32a1423b4a78027ab569b1ea3a9b26802
szyilmaz <semihzafer.yilmaz@yandex.com>
2019-04-03 08:08:32 +0300

    end of section 6

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,18 +1,42 @@
-import React, { Component } from 'react';
-import { Container } from 'semantic-ui-react'
-import EventDashBoard from '../../features/event/EventDashboard/EventDashBoard'
-import NavBar from '../../features/nav/NavBar/NavBar'
+import React, { Component } from "react";
+import { Container } from "semantic-ui-react";
+import { Route, Switch } from "react-router-dom";
+import EventDashBoard from "../../features/event/EventDashboard/EventDashBoard";
+import NavBar from "../../features/nav/NavBar/NavBar";
+import EventForm from "../../features/event/EventForm/EventForm";
+import SettingsDashBoard from "../../features/user/Settings/SettingsDashboard";
+import UserDetailedPage from "../../features/user/UserDetailed/UserDetailedPage";
+import PeopleDashBoard from "../../features/user/PeopleDashboard/PeopleDashboard";
+import EventDetailedPage from "../../features/event/EventDetailed/EventDetailedPage";
+import HomePage from "../../features/home/HomePage";
 
 class App extends Component {
   render() {
     return (
       <div>
-        <NavBar />
-        <Container className="main">
-          <EventDashBoard />
-        </Container>
-      </div>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+        </Switch>
 
+        <Route
+          path="/(.+)"
+          render={() => (
+            <div>
+              <NavBar />
+              <Container className="main">
+                <Switch>
+                  <Route path="/events" component={EventDashBoard} />
+                  <Route path="/event/:id" component={EventDetailedPage} />
+                  <Route path="/people" component={PeopleDashBoard} />
+                  <Route path="/profile/:id" component={UserDetailedPage} />
+                  <Route path="/settings" component={SettingsDashBoard} />
+                  <Route path="/createEvent" component={EventForm} />
+                </Switch>
+              </Container>
+            </div>
+          )}
+        />
+      </div>
     );
   }
 }
