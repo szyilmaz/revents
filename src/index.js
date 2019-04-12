48e6e5d719dbe9a4562c4630c3b76bca1138f638
szyilmaz <semihzafer.yilmaz@yandex.com>
2019-04-12 08:28:44 +0300

    end of section 7

--- src/index.js
+++ src/index.js
@@ -1,32 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import 'semantic-ui-css/semantic.min.css';
 import './index.css';
 import App from './app/layout/App';
-import * as serviceWorker from './serviceWorker';
+import registerServiceWorker from './registerServiceWorker';
+import { configureStore } from './app/store/configureStore';
+
+const store = configureStore();
 
 const rootEl = document.getElementById('root');
 
 let render = () => {
-    ReactDOM.render(
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
-        , rootEl)
-}
+  ReactDOM.render(
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>,
+    rootEl
+  );
+};
 
 if (module.hot) {
-    module.hot.accept('./app/layout/App', () => {
-        setTimeout(render);
-    })
+  module.hot.accept('./app/layout/App', () => {
+    setTimeout(render);
+  });
 }
 
 render();
 
-//ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.register();
+registerServiceWorker();
