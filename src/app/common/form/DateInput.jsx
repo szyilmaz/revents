d6b46d765f0de9172ff40c63153edd648901930c
szyilmaz <semihzafer.yilmaz@yandex.com>
2019-06-10 08:19:29 +0300

    End of sectiın 9

---
+++ src/app/common/form/DateInput.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Form, Label } from 'semantic-ui-react'
+import DatePicker from 'react-datepicker'
+import 'react-datepicker/dist/react-datepicker.css'
+//import moment from 'moment'
+
+const DateInput = ({input: {value, onChange, ...restInput}, width, placeholder, meta: {touched, error}, ...rest}) => {
+  return (
+    <Form.Field error={touched && !!error} width={width}>
+      <DatePicker
+        {...rest}
+        placeholderText={placeholder}
+        selected={value ? new Date(value) : null}
+        //selected={value ? moment(value) : null}
+        onChange={onChange}
+        onChangeRaw={(e) => e.preventDefault()}
+        {...restInput}
+      />
+      {touched && error && <Label basic color='red'>{error}</Label>}
+    </Form.Field>
+  )
+}
+
+export default DateInput
+
+
+//selected={value ? moment(value) : null}
